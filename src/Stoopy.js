c8b18553e9569c9f978006d4f82630049ecdb489
= <v@fractopia.org>
2019-08-16 20:21:07 -0300

    Implement back functionality and refactor

---
+++ src/Stoopy.js
@@ -0,0 +1,128 @@
+import React, { useEffect, useState } from 'react'
+
+import { useFormState } from 'react-use-form-state'
+import get from 'lodash/get'
+import map from 'lodash/map'
+import reduce from 'lodash/reduce'
+import head from 'lodash/head'
+import size from 'lodash/size'
+import find from 'lodash/find'
+import omit from 'lodash/omit'
+import startCase from 'lodash/startCase'
+import filter from 'lodash/filter'
+import CurrentField from './CurrentField'
+import * as defaultLayout from './default/Layout'
+
+export default ({
+  fields: propFields,
+  target,
+  onNext,
+  onEnd,
+  children,
+  saving,
+  progress: progressHandler,
+  title,
+  components = {},
+  ...props
+}) => {
+  const [values, setValues] = useState({})
+  const [formState, fields] = useFormState()
+
+  // Normalize shortened versions into field objects
+  const normalizeFields = fields =>
+    map(fields, (field, index) => {
+      if (typeof field === 'string') {
+        return {
+          name: field,
+          stepKey: index + 1,
+          type: 'text',
+          label: startCase(field),
+        }
+      }
+      return {
+        type: 'text',
+        stepKey: index + 1,
+        label: startCase(field.name),
+        ...field,
+      }
+    })
+
+  // Filter fields, removing those already on target/state
+  const filteredFields = filter(
+    normalizeFields(propFields),
+    field => !get(values, field.name),
+  )
+
+  // Set current field
+  const field = head(filteredFields)
+
+  // Progress
+  const progress = {
+    currentStep: field && field.stepKey,
+    totalSteps: size(normalizeFields(propFields)),
+  }
+
+  if (progressHandler) {
+    progressHandler(progress)
+  }
+
+  // Back functionalities
+  const goBack = ({ stepKey }) => () => {
+    const last = find(normalizeFields(propFields), ['stepKey', stepKey - 1])
+    setValues(omit(values, last.name))
+  }
+
+  // Run function after last field is completed
+  if (!field) {
+    onEnd && onEnd(values)
+  }
+
+  // NOTE: Repetitive code, clean up.
+  const FormHeader = components.FormHeader || defaultLayout.FormHeader
+  const NextButton = components.NextButton || defaultLayout.NextButton
+  const BackButton = components.BackButton || defaultLayout.BackButton
+  const ProgressTracker =
+    components.ProgressTracker || defaultLayout.ProgressTracker
+  const Loading = components.Loading || defaultLayout.Loading
+
+  return saving ? (
+    <Loading />
+  ) : (
+    <>
+      {field && (
+        <form
+          onSubmit={e => {
+            e.preventDefault()
+            // NOTE: Ugly code
+            const value = {}
+            value[field.name] = get(formState.values, field.name)
+            setValues({ ...values, ...value })
+            onNext &&
+              onNext({
+                value: { ...value },
+                values: { ...values, ...value },
+                progress,
+              })
+          }}
+        >
+          <FormHeader progress={progress} title={title} />
+          <CurrentField field={field} fields={fields} formState={formState} />
+          <div
+            style={{
+              display: 'flex',
+              width: '100%',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              marginTop: 20,
+            }}
+          >
+            <BackButton onClick={goBack(field)} />
+            <ProgressTracker progress={progress} />
+            <NextButton />
+          </div>
+        </form>
+      )}
+      {!field && children}
+    </>
+  )
+}
--- src/default/Inputs.js
+++ src/default/Inputs.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react'
 import {
   CircularProgress,
   FormControl,
@@ -11,24 +11,24 @@ import {
   Select,
   TextField,
   Checkbox,
-  InputLabel
-} from "@material-ui/core";
-import { DatePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
-import { DropPicture } from "uploods";
-import startCase from "lodash/startCase";
-import LuxonUtils from "@date-io/luxon";
-import { DateTime } from "luxon";
+  InputLabel,
+} from '@material-ui/core'
+import { DatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers'
+import { DropPicture } from 'uploods'
+import startCase from 'lodash/startCase'
+import LuxonUtils from '@date-io/luxon'
+import { DateTime } from 'luxon'
 
 export const YearInput = props => {
-  return <DateInput views={["year"]} {...props} />;
-};
+  return <DateInput views={['year']} {...props} />
+}
 export const MonthYearInput = props => {
-  return <DateInput views={["month", "year"]} {...props} />;
-};
+  return <DateInput views={['month', 'year']} {...props} />
+}
 
 export const TimeInput = props => {
-  return null;
-};
+  return null
+}
 
 export const DateInput = ({
   label,
@@ -40,7 +40,7 @@ export const DateInput = ({
   ...props
 }) => {
   const normalizedValue =
-    value === "" ? DateTime.local() : DateTime.fromISO(value);
+    value === '' ? DateTime.local() : DateTime.fromISO(value)
   return (
     <MuiPickersUtilsProvider utils={LuxonUtils}>
       <DatePicker
@@ -51,8 +51,8 @@ export const DateInput = ({
         value={normalizedValue}
       />
     </MuiPickersUtilsProvider>
-  );
-};
+  )
+}
 
 export const CheckboxInput = ({
   label,
@@ -74,8 +74,8 @@ export const CheckboxInput = ({
         />
       </FormGroup>
     </FormControl>
-  );
-};
+  )
+}
 
 // What to do about booleans in values transformed to string?
 export const RadioInput = ({ label, choices, ...props }) => {
@@ -85,26 +85,25 @@ export const RadioInput = ({ label, choices, ...props }) => {
       <RadioGroup {...props}>
         {choices.map(choice => {
           const opts =
-            typeof choice === "object"
+            typeof choice === 'object'
               ? choice
               : {
                   label: choice,
-                  value: choice
-                };
+                  value: choice,
+                }
           return (
             <FormControlLabel
               key={choice.label}
               control={<Radio />}
               {...opts}
             />
-          );
+          )
         })}
       </RadioGroup>
     </FormControl>
-  );
-};
+  )
+}
 export const SelectInput = ({ label, value, choices, ...props }) => {
-  console.log("value", value);
   return (
     <FormControl style={{ minWidth: 120 }}>
       <InputLabel>{label}</InputLabel>
@@ -119,33 +118,34 @@ export const SelectInput = ({ label, value, choices, ...props }) => {
         ))}
       </Select>
     </FormControl>
-  );
-};
+  )
+}
 
 export const DropAvatar = props => {
   return (
-    <DropPicture
-      maxDimension={200}
-      config={{
-        apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-        storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET
-      }}
-      {...props}
-    />
-  );
-};
+    <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+      <DropPicture
+        maxDimension={200}
+        config={{
+          apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+          storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+        }}
+        {...props}
+      />
+    </div>
+  )
+}
 
 export const Input = ({
   disabled,
   name,
   error,
-  helper = " ",
+  helper = ' ',
   label = startCase(name),
   placeholder = label,
   ...props
 }) => {
-  console.log("props", props);
-  const helperText = error || helper;
+  const helperText = error || helper
   return (
     <TextField
       {...props}
@@ -158,8 +158,8 @@ export const Input = ({
       helperText={helperText}
       disabled={disabled}
       style={{
-        marginBottom: 10
+        marginBottom: 10,
       }}
     />
-  );
-};
+  )
+}
