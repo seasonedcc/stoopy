3bf89d2b2da3ed39b8f1ea0b894ffdf27104f5e9
= <v@fractopia.org>
2019-09-19 19:10:22 -0300

    Remove more specific defaults to reduce package size

--- src/fieldTypes.js
+++ src/fieldTypes.js
@@ -1,14 +1,5 @@
 import get from 'lodash/get'
-import {
-  SelectInput,
-  CheckboxInput,
-  RadioInput,
-  DropAvatar,
-  DateInput,
-  YearInput,
-  MonthYearInput,
-  Input,
-} from './default/Inputs'
+import { SelectInput, CheckboxInput, RadioInput, Input } from './default/Inputs'
 
 // Get correct component and field type from type provided
 const parseType = (type, fields) => {
@@ -16,76 +7,24 @@ const parseType = (type, fields) => {
     text: {
       Component: Input,
       field: fields.text,
-      baseOpts: {
-        validate: e => {
-          return e.length > 0
-        },
-      },
+      validate: value => value.length > 0,
     },
     select: {
       Component: SelectInput,
       field: fields.select,
-      baseOpts: {
-        onChange: e => e.target.value,
-      },
+      onChange: e => e.target.value,
     },
     checkbox: {
       Component: CheckboxInput,
       field: fields.checkbox,
       optional: true,
-      baseOpts: {
-        onChange: e => e.target.value,
-      },
+      onChange: e => e.target.value,
     },
     radio: {
       Component: RadioInput,
       field: fields.radio,
-      baseOpts: {
-        onChange: e => e.target.value,
-      },
+      onChange: e => e.target.value,
     },
-    // TODO: Finish/improve DateTime related fields
-    date: {
-      Component: DateInput,
-      field: fields.raw,
-      baseOpts: {
-        // NOTE: Erase time?
-        onChange: dateTime => dateTime.toUTC().toString(),
-      },
-    },
-    year: {
-      Component: YearInput,
-      field: fields.raw,
-      baseOpts: {
-        onChange: dateTime => {
-          return dateTime.c.year
-        },
-      },
-    },
-    yearMonth: {
-      Component: MonthYearInput,
-      field: fields.raw,
-      baseOpts: {
-        // NOTE: Erase time?
-        onChange: dateTime => dateTime.toUTC().toString(),
-      },
-    },
-    // time: {
-    //   Component: TimeInput,
-    //   field: fields.raw,
-    //   baseOpts: {
-    //     onChange: dateTime => dateTime.toUTC().toString()
-    //   }
-    // },
-    avatar: {
-      Component: DropAvatar,
-      field: fields.raw,
-      baseOpts: {
-        onChange: file => file.url,
-      },
-    },
-    // images: { Component: ImagesComponent, field: 'raw' },
-    // files: { Component: FilesComponent, field: 'raw' },
   }
 
   return get(list, type, {
